4cc1fd2019d44f357c4a27c1b431355002968983
glynev <gdjevans@gmail.com>
2021-10-23 23:16:58 +0100

    Starting console app

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,8 @@
+﻿using System; // Using is a keyword, System is a namespace, using System adds external code
+class Program //1. This line gives structure to our program because of the design of C#
+{
+    static void Main () //2. Main is required in ordere for the program to run
+    {
+        Console.WriteLine("Hello , World!");
+    }
+}
