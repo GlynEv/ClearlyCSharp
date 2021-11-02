ab7d3583adef7ca8c93ee5c1f831fd5772ef3611
glynev <gdjevans@gmail.com>
2021-11-02 04:11:51 +0000

    Complete Sophisticated C# Section

---
+++ ConsoleApp07/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace ConsoleApp07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 17, y = 5; //Declares and sets twio integers to illustrate division.
+
+            // Divides x and y, and stores the results to resultOne as an integer.
+            int resultOne = x / y;
+            //Prints the resultOne variable to console.
+            WriteLine($"{y} goes into {x}, {resultOne} times whole");
+
+            double a = 17, b = 5; 
+            double resultTwo = a / b;
+            WriteLine($"{a}/{b}={resultTwo}");
+
+
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace ConsoleApp08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal bobSalary = 65000, jerrySalary = 70000, sallySalary = 75000;
+            decimal averageSalary = (bobSalary + jerrySalary + sallySalary) / 3;
+            WriteLine($"The average salary at company ABC is {averageSalary}");
+
+        }
+    }
+}
